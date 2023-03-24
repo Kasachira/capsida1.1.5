f1995e13cfd8300e664a3a230605bad31c56a17d
Kasachira <118664623+Kasachira@users.noreply.github.com>
2023-03-24 10:16:17 +0900

    25/02

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,24 +2,26 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main  {
     public static void main(String[] args) {
         Util.getConnection();
-        UserServiceImpl createUsersTable = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
 
-        createUsersTable.createUsersTable();
 
-        createUsersTable.saveUser("Name1", "LastName1", (byte) 20);
-        createUsersTable.saveUser("Name2", "LastName2", (byte) 25);
-        createUsersTable.saveUser("Name3", "LastName3", (byte) 31);
-        createUsersTable.saveUser("Name4", "LastName4", (byte) 38);
+        userService.createUsersTable();
 
-        createUsersTable.removeUserById(1);
-        createUsersTable.getAllUsers();
-        createUsersTable.cleanUsersTable();
-        createUsersTable.dropUsersTable();
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
